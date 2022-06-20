a5f0aeae5762cda0461a12efe673b25a3f7382b5
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2022-06-20 09:55:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,14 +28,16 @@ face = Face()
 
 app = FastAPI()
 
-#CORS enable
+origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 class Info(BaseModel):
@@ -58,7 +60,6 @@ async def encode(file: UploadFile = File(...)):
     encoding = face.encode(temp.name)
     print(type(encoding))
     temp.close()
-#     response.headers.add('Access-Control-Allow-Origin', '*')
     return {"Task" : "Image Encoding", "Status" : True, "encoding" : list(encoding)}
 
 
